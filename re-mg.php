4f6c62cd80ab2c76dc732ef4251071bf50d4fe4b
MG555TH <98292320+MG555TH@users.noreply.github.com>
2022-03-09 11:28:28 +0700

    Create re-mg.php

---
+++ re-mg.php
@@ -0,0 +1,466 @@
+<!DOCTYPE html>
+<html lang="th" prefix="og: https://ogp.me/ns#">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <!-- Search Engine Optimization by Rank Math PRO - https://s.rankmath.com/home -->
+    <title>MG555TH สล็อตเว็บตรง 2022 ไม่ผ่านเอเย่นต์ ล่าสุด ฝากถอน ไม่มีขั้นต่ำ </title>
+    <meta name="description"
+        content="สล็อตเว็บตรง แตกง่าย สมัครสล็อต เว็บตรงสล็อต ไม่ผ่านเอเย่นต์ รวมทุกค่าย ฝากถอน ไม่มีขั้นต่ำ สมัครสมาชิกเล่น SLOT ONLINE เว็บตรง รวมค่าย ล่าสุด 2022" />
+    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+    <link rel="canonical" href="https://slot789pro.com/register/" />
+    <meta property="og:locale" content="th_TH" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="สล็อตเว็บตรง 2022 ไม่ผ่านเอเย่นต์ ล่าสุด ฝากถอน ไม่มีขั้นต่ำ ฟรี 500" />
+    <meta property="og:description"
+        content="สล็อตเว็บตรง แตกง่าย สมัครสล็อต เว็บตรงสล็อต ไม่ผ่านเอเย่นต์ รวมทุกค่าย ฝากถอน ไม่มีขั้นต่ำ สมัครสมาชิกเล่น SLOT ONLINE เว็บตรง รวมค่าย ล่าสุด 2022" />
+    <meta property="og:url" content="https://slot789pro.com/register/" />
+    <meta property="og:site_name" content="เว็บสูตรสล็อต เว็บตรง" />
+    <meta property="og:updated_time" content="2022-03-08T11:18:07+00:00" />
+    <meta property="og:image"
+        content="https://slot789pro.com/wp-content/uploads/2021/12/1.สล็อตเว็บตรง-ไม่ผ่าน-เอเย่น-ฝากถอนไม่มีขั้นต่ำ.png" />
+    <meta property="og:image:secure_url"
+        content="https://slot789pro.com/wp-content/uploads/2021/12/1.สล็อตเว็บตรง-ไม่ผ่าน-เอเย่น-ฝากถอนไม่มีขั้นต่ำ.png" />
+    <meta property="og:image:width" content="550" />
+    <meta property="og:image:height" content="550" />
+    <meta property="og:image:alt" content="สล็อต เว็บตรงไม่ผ่านเอเย่นต์ไม่มีขั้นต่ำ" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="article:published_time" content="2021-06-01GMT+000015:02:16+00:00" />
+    <meta property="article:modified_time" content="2022-03-08GMT+000011:18:07+00:00" />
+    <meta property="og:video" content="https://www.youtube.com/embed/q76eIbzbWPI" />
+    <meta property="video:duration" content="91" />
+    <meta property="og:video" content="https://www.youtube.com/embed/piMg0wj460s" />
+    <meta property="video:duration" content="139" />
+    <meta property="ya:ovs:upload_date" content="2021-07-29" />
+    <meta property="ya:ovs:allow_embed" content="true" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="สล็อตเว็บตรง 2022 ไม่ผ่านเอเย่นต์ ล่าสุด ฝากถอน ไม่มีขั้นต่ำ ฟรี 500" />
+    <meta name="twitter:description"
+        content="สล็อตเว็บตรง แตกง่าย สมัครสล็อต เว็บตรงสล็อต ไม่ผ่านเอเย่นต์ รวมทุกค่าย ฝากถอน ไม่มีขั้นต่ำ สมัครสมาชิกเล่น SLOT ONLINE เว็บตรง รวมค่าย ล่าสุด 2022" />
+    <meta name="twitter:image"
+        content="https://slot789pro.com/wp-content/uploads/2021/12/1.สล็อตเว็บตรง-ไม่ผ่าน-เอเย่น-ฝากถอนไม่มีขั้นต่ำ.png" />
+    <meta name="twitter:label1" content="Time to read" />
+    <meta name="twitter:data1" content="Less than a minute" />
+
+
+    <link rel='stylesheet' id='s-bootstrap-css'
+        href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css?v=1.0&#038;ver=b229bc030962eaa058c3f57d06f38e3d'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='s-mobile-css'
+        href='https://slot789pro.com/wp-content/themes/seed/css/mobile.css?ver=2.0.7' type='text/css' media='all' />
+    <link rel='stylesheet' id='s-desktop-css'
+        href='https://slot789pro.com/wp-content/themes/seed/css/desktop.css?ver=2.0.7' type='text/css'
+        media='(min-width: 992px)' />
+    <link rel='stylesheet' id='s-ie-css' href='https://slot789pro.com/wp-content/themes/seed/css/ie.css?ver=2.0.7'
+        type='text/css' media='(-ms-high-contrast: none), (-ms-high-contrast: active)' />
+    <link rel='stylesheet' id='s-soots-css' href='https://slot789pro.com/wp-content/themes/seed/css/soot.css?ver=2.0.7'
+        type='text/css' media='all' />
+    <link rel='stylesheet' id='generateblocks-css'
+        href='https://slot789pro.com/wp-content/uploads/generateblocks/style-2421.css?ver=1646738288' type='text/css'
+        media='all' />
+    <link rel='stylesheet' id='kadence-blocks-rowlayout-css'
+        href='https://slot789pro.com/wp-content/plugins/kadence-blocks/dist/blocks/row.style.build.css?ver=2.3.0'
+        type='text/css' media='all' />
+    <style id='kb-rowlayout_753e21-d5-inline-css' type='text/css'>
+        #kt-layout-id_753e21-d5>.kt-row-column-wrap {
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+    </style>
+
+    <style id='kb-rowlayout_ab27f0-af-inline-css' type='text/css'>
+        #kt-layout-id_ab27f0-af>.kt-row-column-wrap {
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+    </style>
+    <style id='kb-rowlayout_6caffd-56-inline-css' type='text/css'>
+        #kt-layout-id_6caffd-56>.kt-row-column-wrap {
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+    </style>
+    <style id='kb-rowlayout_d6bc7f-3e-inline-css' type='text/css'>
+        #kt-layout-id_d6bc7f-3e>.kt-row-column-wrap {
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+    </style>
+    <style id='kb-rowlayout_39f7f6-f9-inline-css' type='text/css'>
+        #kt-layout-id_39f7f6-f9>.kt-row-column-wrap {
+            padding-top: 0px;
+        }
+    </style>
+    <style id='kb-rowlayout_893f65-da-inline-css' type='text/css'>
+        #kt-layout-id_893f65-da>.kt-row-column-wrap {
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+    </style>
+    <style id='kb-rowlayout_7356b1-d5-inline-css' type='text/css'>
+        #kt-layout-id_7356b1-d5>.kt-row-column-wrap {
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+    </style>
+    <style id="kadence_blocks_palette_css">
+        :root .has-kb-palette-2-color {
+            color: #f70808
+        }
+
+        :root .has-kb-palette-2-background-color {
+            background-color: #f70808
+        }
+
+        :root .has-kb-palette-3-color {
+            color: #ffd608
+        }
+
+        :root .has-kb-palette-3-background-color {
+            background-color: #ffd608
+        }
+
+        :root .has-kb-palette-4-color {
+            color: #31f300
+        }
+
+        :root .has-kb-palette-4-background-color {
+            background-color: #31f300
+        }
+
+        :root .has-kb-palette-5-color {
+            color: #1e00ff
+        }
+
+        :root .has-kb-palette-5-background-color {
+            background-color: #1e00ff
+        }
+
+        :root .has-kb-palette-1-color {
+            color: #ce1e35
+        }
+
+        :root .has-kb-palette-1-background-color {
+            background-color: #ce1e35
+        }
+    </style>
+
+
+    <meta name="generator" content="Site Kit by Google 1.69.0" />
+    <style>
+        #h5vp_endscreen .endscreen_content button {
+            color: #ffffff;
+            background: #00b3ff
+        }
+
+        #h5vp_endscreen .endscreen_content button:hover {
+            color: #ffffff;
+            background: #19baff
+        }
+
+        #h5vp_endscreen .endscreen_content p a {
+            color: #fff;
+        }
+    </style>
+    <style>
+        #h5vpQuickPlayer {
+            width: 100%;
+            max-width: 100%;
+            margin: 0 auto;
+        }
+    </style>
+
+
+    <!-- End Google Tag Manager snippet added by Site Kit -->
+    <link rel="icon" href="assets/images/T1-LOgoMG555TH.png.png" sizes="32x32" />
+    <link rel="icon" href="assets/images/T1-LOgoMG555TH.png.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="assets/images/T1-LOgoMG555TH.png.png" />
+    <meta name="msapplication-TileImage" content="assets/images/T1-LOgoMG555TH.png.png" />
+    <style type="text/css"></style>
+    <style type="text/css" id="wp-custom-css">
+        a:hover {
+            text-decoration: none;
+        }
+
+        .bg-grid {
+            background: linear-gradient(0deg, black, #222);
+            padding: 16px;
+            border-radius: 0 0 10px 10px;
+            margin-bottom: 16px;
+        }
+
+        .ml-16 {
+            margin-left: -16px;
+        }
+
+        .site-header {
+            background-color: #ce1e35;
+        }
+
+        .page .-contact,
+        .single .-contact {
+            display: none;
+        }
+
+        .home .-contact {
+            display: block;
+        }
+
+        .-contact {
+            margin: 0 auto;
+            margin-bottom: 15px;
+            text-align: center;
+        }
+
+        .text-primary {
+            color: #ce1e35 !important;
+        }
+
+        .btn.btn-primary.btn-color-gradient {
+            background: linear-gradient(0deg, #E5472A 28.13%, #ED7133 74.48%);
+            border: 0;
+        }
+
+        .main-header .bg.-blank {
+            background: -webkit-linear-gradient(326deg, #861422 0, #eb2e47 45%, #861422 100%);
+            background: -o-linear-gradient(326deg, #861422 0, #eb2e47 45%, #861422 100%);
+            background: linear-gradient(124deg, #861422 0, #eb2e47 45%, #861422 100%);
+        }
+
+        .-menu .s-container a:after {
+            content: none;
+            display: none;
+        }
+
+        .page-id-2427 .-sec-login {
+            display: none !important;
+        }
+
+        .site-footer-space {
+            min-height: 70px;
+        }
+
+        .content-item.-card {
+            padding-bottom: 16px;
+        }
+
+        @media (min-width: 992px) {
+            .main-header .title {
+                font-size: 1.35rem;
+            }
+
+            .site-nav-d li a {
+                padding: 10px;
+            }
+
+            .site-header-space {
+                min-height: 100px;
+            }
+
+            h2 {
+                font-size: 1.5em;
+            }
+
+            .site-header>.s-container {
+                position: relative;
+                min-height: 0;
+                display: unset;
+                flex-wrap: unset;
+                align-items: unset;
+            }
+
+            .site-header a:hover {
+                color: #ad3;
+            }
+
+            .site-header {
+                display: flex;
+                align-items: center
+            }
+
+            .main-header.-banner {
+                min-height: 150px;
+            }
+
+            .site-nav-d li a {
+                font-size: 14px;
+                font-weight: 300;
+            }
+        }
+
+        @media (min-width: 768px) {
+            .main-header .title {
+                font-size: 1.2rem;
+                margin: 0;
+            }
+
+            .wp-block-image {
+                margin-top: 0;
+            }
+        }
+
+        @media (min-width: 320px) {
+            .main-header.-banner {
+                padding-bottom: 0;
+                min-height: 0;
+                padding-bottom: 14px;
+            }
+        }
+
+        @media (max-width: 767.98px) {
+            .site-footer-space {
+                min-height: 200px;
+            }
+
+            .site-footer {
+                bottom: 200px;
+            }
+
+            #chat-widget-container {
+                bottom: 55px !important;
+                right: 0px !important;
+                max-height: calc(100% - 90px) !important;
+            }
+
+            .main-header .title {
+                font-size: 1.0rem;
+                margin: 0;
+            }
+
+            .main-header.-banner {
+                min-height: 0;
+            }
+
+            .-contact {
+                width: 100%;
+            }
+
+            .site-branding {
+                transform: translate(-50%, 0%);
+            }
+
+            h1 {
+                font-size: 1.4em;
+            }
+
+            h2 {
+                font-size: 1.2em;
+            }
+
+            h3 {
+                font-size: 1.1em;
+            }
+        }
+    </style>
+
+</head>
+
+
+
+<div class="main-header -banner">
+    <div class="bg -blank"></div>
+    <div class="s-container">
+        <div class="main-title _heading">
+            <div class="title">
+                <a href="#">
+                    <h1 class="title">สล็อตเว็บตรง ไม่ผ่านเอเย่นต์ ฝากถอน ไม่มีขั้นต่ำ สมัครสมาชิก เว็บตรง 2022</h1>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="s-container main-body">
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main -hide-title">
+
+
+            <article id="post-2421" class="post-2421 page type-page status-publish has-post-thumbnail hentry">
+
+                <header class="entry-header">
+                </header>
+
+                <div class="entry-content">
+
+
+
+
+
+                    <h2 class="has-text-align-center"
+                        id="สล-อตเว-บตรง-ไม-ผ-านเอเย-นต-ล-าส-ด-สม-ครสมาช-ก-กรอกข-อม-ลสม-ครด-านล-าง"> สล็อตเว็บตรง
+                        ไม่ผ่านเอเย่นต์ ล่าสุด สมัครสมาชิก กรอกข้อมูลสมัครด้านล่าง</h2>
+
+
+
+
+                   
+                    <div class="bg-register p-4 border-r-10">
+                        <form id="register-form">
+                            <!-- <div class="text-primary text-center mb-1" style="font-size: 34px">สมัครสมาชิก</div> -->
+                            <form action="send.php">
+                            <div class="form-group"><label class="text-white" for="register-tel">เบอร์โทร</label><input
+                                    class="form-control use-validate-sys"  type="text" name="phoneS"
+                                    placeholder="เบอร์โทร" name="tel">
+                            </div>
+                            <div class="form-group"><label class="text-white" for="register-pass">รหัสผ่าน</label><input
+                                    class="form-control use-validate-sys" id="register-pass" type="password"
+                                    placeholder="รหัสผ่าน" name="password"></div>
+                            <div class="form-group"><label class="text-white" for="register-fname">ชื่อ -
+                                    นามสกุล</label><small class="text-primary"
+                                    for="register-lname">**ชื่อต้องตรงกับบัญชีที่สมัครและเป็นภาษาไทยเท่านั้น</small>
+                                <div class="row no-gutters">
+                                    <div class="col-12"><input class="form-control mb-3 use-validate-sys"
+                                            id="register-fname" type="text" placeholder="ชื่อภาษาไทย" name="fname">
+                                    </div>
+                                    <div class="col-12"><input class="form-control use-validate-sys" id="register-lname"
+                                            type="text" placeholder="นามสกุลภาษาไทย" name="lname"></div>
+                                </div>
+                            </div>
+                            <div class="form-group"><label class="text-white" for="lineRegister">Line ID</label><input
+                                    class="form-control use-validate-sys" id="lineRegister" type="text"
+                                    placeholder="ไอดีไลน์" name="line">
+                            </div>
+                            <div class="form-group"><label class="text-white"
+                                    for="register-bankname">ธนาคาร</label><select class="form-control use-validate-sys"
+                                    id="register-bankname" name="bankname">
+                                    <option value="" selected="">เลือกธนาคาร</option>
+                                    <option value="KBANK">ธนาคารกสิกรไทย</option>
+                                    <option value="BBL">ธนาคารกรุงเทพ</option>
+                                    <option value="BAY">ธนาคารกรุงศรีอยุธยา</option>
+                                    <option value="SCB">ธนาคารไทยพาณิชย์</option>
+                                    <option value="KTB">ธนาคารกรุงไทย</option>
+                                    <option value="SCIB">ธนาคารนครหลวงไทย</option>
+                                    <option value="UOB">ธนาคารยูโอบี</option>
+                                    <option value="TMB">ธนาคารทหารไทย</option>
+                                    <option value="TISCO">ธนาคารทิสโก้</option>
+                                    <option value="ICBC">ธนาคารไอซีบีซี (ไทย)</option>
+                                    <option value="KKP">ธนาคารเกียรตินาคิน</option>
+                                    <option value="TBANK">ธนาคารธนชาต</option>
+                                    <option value="Standard Chartered">ธนาคารสแตนดาร์ดชาร์เตอร์ดไทย</option>
+                                    <option value="GHB">ธนาคารอาคารสงเคราะห์</option>
+                                    <option value="LHB">ธนาคารแลนด์ แอนด์ เฮาส์</option>
+                                    <option value="GSB">ธนาคารออมสิน</option>
+                                    <option value="BAC">ธนาคารเพื่อการเกษตรและสหกรณ์การเกษตร (ธกส)</option>
+                                    <option value="Islamic">ธนาคารอิสลามแห่งประเทศไทย</option>
+                                    <option value="CIMB">ธนาคารซีไอเอ็มบีไทย</option>
+                                </select></div>
+                            <div class="form-group"><label class="text-white"
+                                    for="register-bankacc">เลขบัญชีธนาคาร</label><input
+                                    class="form-control use-validate-sys" id="register-bankacc" type="text"
+                                    placeholder="เลขบัญชีธนาคาร" name="bankaccount">
+                                <div class="text-center" id="resgister-resp"
+                                    style="width:100%; border:1px; padding:5px; font-size:14px; background:rgb(255,0,0,0.5); margin-top:10px; border-radius:10px; display:none">
+                                </div>
+                            </div>
+                            <div class="form-group pt-2"><a
+                                    class="btn btn-lg btn-primary btn-color-gradient btn-block border-r-10"
+                                    id="register-btn-submit" href="#" rel="nofollow">ยืนยันการสมัคร</a></div>
+                                    </form>
+                        </form>
+                    </div>
+                    </body>
+
+</html>
